8585c57de425bc946e1eb963fa74e06d3cb5b830
Connell Gardner <cgardn14@live.spcollege.edu>
2019-12-09 19:34:24 -0500

    Big old commit!

---
+++ DataCollector/MainDisplayData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCollector
+{
+    class MainDisplayData
+    {
+        private int kph;
+        public int Kph
+        {
+            get { return this.kph; }
+            set { this.kph = value; }
+        }
+
+        private string displayArray;
+        public string DisplayArray
+        {
+            get { return this.displayArray; }
+            set { this.displayArray = value; }
+        }
+
+        private string displayArrayImperial;
+        public string DisplayArrayImperial
+        {
+            get { return this.displayArrayImperial; }
+            set { this.displayArrayImperial = value; }
+        }
+    }
+}
--- DataCollector/MainPage.xaml.cs
+++ DataCollector/MainPage.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -19,9 +22,16 @@ namespace DataCollector
     public sealed partial class MainPage : Page
     {
         MeasureLengthDevice myMLD = new MeasureLengthDevice();
+        MainDisplayData displayData = null;
+        private Timer timer;
         public MainPage()
         {
             this.InitializeComponent();
+            displayData = new MainDisplayData();
+
+            timer = new Timer(timer_Tick, null, 
+                (int)TimeSpan.FromSeconds(1).TotalMilliseconds, 
+                (int)TimeSpan.FromSeconds(3).TotalMilliseconds);
 
         }
 
@@ -44,5 +54,20 @@ namespace DataCollector
             singleMeasuretextBlock.Text = myMLD.GetMostRecentMeasure().ToString();
 
         }
+
+        private async void timer_Tick(object state)
+        {
+            //code to randomly generate a new value and update GetMeasurement
+            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+            () =>
+            {
+                displayData.Kph = myMLD.GetMostRecentMeasure();
+                displayData.DisplayArray = myMLD.MetricValue();
+                displayData.DisplayArrayImperial = myMLD.ImperialValue();
+                this.DataContext = null;
+                this.DataContext = displayData;
+            });
+        }
+
     }
 }
